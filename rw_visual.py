bfaa9fae55baea0956df882e995a7a084ce108e1
Paavo Vuorilehto <paavo.vuorilehto@valagroup.com>
2022-07-28 21:08:08 +0300

    Multiple plots

--- rw_visual.py
+++ rw_visual.py
@@ -4,12 +4,20 @@ import matplotlib.pyplot as plt
 
 from random_walk import RWalkGenerator
 
-#Make a random walk
-my_walker = RWalkGenerator()
-my_walker.fill_walk()
+while True:
+    #Make a random walk
+    my_walker = RWalkGenerator()
 
-#Plot the points in the walk
-plt.style.use("fivethirtyeight")
-fig, ax = plt.subplots()
-ax.scatter(my_walker.x_coordinate, my_walker.y_coordinate, s=15)
-plt.show()
+
+    my_walker.fill_walk()
+
+    #Plot the points in the walk
+    plt.style.use("fivethirtyeight")
+    fig, ax = plt.subplots()
+    ax.scatter(my_walker.x_coordinate, my_walker.y_coordinate, s=15)
+    plt.show()
+
+    #Ask for repeat
+    another_round = input("Another one (y/n)?")
+    if another_round == "n":
+        break
