7e4909f01c6a9de9db00d40c7aae036e84a5e93b
Paavo Vuorilehto <paavo.vuorilehto@valagroup.com>
2022-07-29 16:28:37 +0300

    Scatter-chart enhancements

--- rw_visual.py
+++ rw_visual.py
@@ -14,7 +14,16 @@ while True:
     #Plot the points in the walk
     plt.style.use("fivethirtyeight")
     fig, ax = plt.subplots()
+    coordinate_points = range(my_walker.num_points)
     ax.scatter(my_walker.x_coordinate, my_walker.y_coordinate, s=15)
+    #c = coordinate_points, cmap = plt.cm.Blues,
+    ax.scatter(my_walker.x_coordinate[0], my_walker.y_coordinate[0], c = "red", s = 100)
+    ax.scatter(my_walker.x_coordinate[-1], my_walker.y_coordinate[-1], c = "green", s = 100)
+    ax.axis([min(my_walker.x_coordinate) - 20, max(my_walker.x_coordinate) + 20, min(my_walker.y_coordinate) - 20, max(my_walker.y_coordinate) + 20])
+    ax.set_title("Random walk illustration with 5000 steps")
+    ax.set_xlabel("X-coordinate")
+    ax.set_ylabel("Y-coordinate")
+    
     plt.show()
 
     #Ask for repeat
